c7de98f7ce821a20ea814dede1a566d0a0a7ae60
Tushar@7898 <tusharingle63@gmail.com>
2023-05-05 01:38:46 +0530

    all data changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,110 @@
-import React from 'react';
-import './App.css';
+import { useState } from 'react';
+ 
+export default function Form() {
+ 
+  // States for registration
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+ 
+  // States for checking the errors
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
+ 
+  // Handling the name change
+  const handleName = (e) => {
+    setName(e.target.value);
+    setSubmitted(false);
+  };
+ 
+  // Handling the email change
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    setSubmitted(false);
+  };
+ 
+  // Handling the password change
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    setSubmitted(false);
+  };
+ 
+  // Handling the form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name === '' || email === '' || password === '') {
+      setError(true);
+    } else {
+      setSubmitted(true);
+      setError(false);
+    }
+  };
+ 
+  // Showing success message
+  const successMessage = () => {
+    return (
+      <div
+        className="success"
+        style={{
+          display: submitted ? '' : 'none',
+        }}>
+        <h1>User {name} successfully registered!!</h1>
+      </div>
+    );
+  };
+ 
+  // Showing error message if error is true
+  const errorMessage = () => {
+    return (
+      <div
+        className="error"
+        style={{
+          display: error ? '' : 'none',
+        }}>
+        <h1>Please enter all the fields</h1>
+      </div>
+    );
+  };
+ 
+  return (
+    <div className="form">
+      <div>
+        <h1>User Registration</h1>
+      </div>
+ 
+      {/* Calling to the methods */}
+      <div className="messages">
+        {errorMessage()}
+        {successMessage()}
+      </div>
+ 
+      <form>
+        {/* Labels and inputs for form data */}
+        <label className="label">Name</label>
+        <br/><br/>
+        <input onChange={handleName} className="input"
+          value={name} type="text" />
 
+              <br/><br/>
+ 
+        <label className="label">Email</label>
+        <br/><br/>
+        <input onChange={handleEmail} className="input"
+          value={email} type="email" />
 
-function App() {
-  return 
-   <span>Input Form</span>
+          <br/><br/>
+ 
+        <label className="label">Password</label>
+        <br/><br/>
+        <input onChange={handlePassword} className="input"
+          value={password} type="password" />
+
+                  <br/><br/>
+ 
+        <button onClick={handleSubmit} className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
-
-export default App;
